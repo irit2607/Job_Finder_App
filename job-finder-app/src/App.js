02b2ebb39e5297f7889debd6e92d09c3e7f38ae7
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-08-16 20:38:32 +0530

    post-item

--- job-finder-app/src/App.js
+++ job-finder-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import NavBar from './components/NavBar';
+import PostItems from './components/PostItems';
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <PostItems/>
     </div>
   )
 }
---
+++ job-finder-app/src/components/PostItems.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Container, Button, Card ,Form, Col,Row} from 'react-bootstrap';
+
+const PostItems = () => {
+    return (
+        <div>
+            <Card className="text-center" style={{ margin: '10px 160px 15px' }}>
+                <Card.Header>
+                    <Row >
+                        <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label style={{textAlign:'left'}}>Company Name</Form.Label>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Time | Full Time | Remote</Form.Label>
+                        </Form.Group>
+                       
+                    </Row>
+                    <Row>
+                
+                    <Button style={{ backgroundColor: "purple", marginLeft: '25px', height: '35px', width: '8rem' }}>Job title</Button>{' '}
+                    <Form.Group >
+                    <Button variant="outline-primary">JavaScript</Button>{' '}
+    <Button variant="outline-primary">React</Button>{' '}
+                        </Form.Group>
+                    
+                        <Button variant="outline-primary" style={{borderRadius:'20px', marginLeft: '25px', height: '35px', width: '8rem' }}>Check</Button>{' '}
+                    
+                    </Row>
+                </Card.Header>
+
+            </Card>
+        </div>
+    )
+}
+
+export default PostItems
